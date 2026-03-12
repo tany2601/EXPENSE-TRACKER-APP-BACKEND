14c399a85a47fd4563b98f2975543a5c8384f357
Thanay <thanayab08@gmail.com>
2026-03-13 01:01:25 +0530

    Add CORS origin for api.rupexo.paperlighttech.com

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
     origin: corsOrigins.length
       ? corsOrigins
       : [
+        'https://api.rupexo.paperlighttech.com',
         'http://localhost:5173',
         'http://localhost:8080',
         'http://localhost:3000',
